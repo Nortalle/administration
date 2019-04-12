4dda8dd57a836c3f97679eed41a41ed4fa874b5f
Nortalle <vincent.guidoux1@heig-vd.ch>
2019-04-12 17:16:46 +0200

    Where is the right 13 displays

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ function postApi(endpoint, payload) {
 
 function tableCreate() {
     const disposition = [
-        [true, false, true, false, true, true, true, true],
+        [true, false, true, false, true, true, true, false],
         [false, false, true, true, false, false, true, true],
         [false, false, false, false, true, true, false, true],
         [false, false, false, false, false, false, true, false],
